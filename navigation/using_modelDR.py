9301ca6fa0e6830d68b0ccbc256a466d82882328
Maryam-Elmorshidy <engotta2002@gmail.com>
2024-05-22 09:45:01 +0300

    Update using_modelDR.py

--- navigation/using_modelDR.py
+++ navigation/using_modelDR.py
@@ -78,38 +78,3 @@ def use_page():
 
 
 
-    #______________________________________________________________________________________________
-        # #uploaded_file = st.file_uploader("upload image")
-        # file = st.file_uploader(' ### upload  image', type=['jpeg', 'jpg', 'png'])
-
-        # classification = st.button("Classify")
-        # if classification : 
-        #     pass
-
-    #_______________________________________________________________________________________________
-        # example to solution
-        # #load classifier 
-        # model = load_model('./Streamlit5/model/pneumonia_classifier.h5')
-
-        # # load class names
-        # with open('./Streamlit5/model/labels.txt', 'r') as f:
-        #     class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
-        #     f.close()
-
-        # # display image
-        # if file is not None:
-        #     image = Image.open(file).convert('RGB')
-        #     st.image(image, use_column_width=True)
-
-        #     # classify image
-        #     class_name, conf_score = classify(image, model, class_names)
-
-        #     # write classification
-        #     st.write("## {}".format(class_name))
-        #     st.write("### score: {}%".format(int(conf_score * 1000) / 10))
-
-
-
-
-
-
